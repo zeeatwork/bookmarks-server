57e8ba0784fde2525670df529dd69b4ff5331d79
Zenzi Ali <zeeathome935@gmail.com>
2020-08-20 13:34:04 -0400

    created migration and seed data

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,12 @@
+INSERT INTO bookmarks (title, url, description, rating )
+VALUES
+ ('Google', 'http://www.google.com', 'my link to google', 53),
+  ('Yahoo', 'http://www.yahoo.com', 'my link to yahoo', 5),
+  ('Pinterest', 'http://www.pinterest.com', 'my link to yahoo', 3),
+  ('Twitter', 'http://www.twitter.com', 'my link to Twitter', 1),
+  ('Facebook', 'http://www.facebook.com', 'my link to Facebook', 5),
+  ('Instagram', 'http://www.instagram.com', 'my link to Instagram', 4),
+  ('zillow', 'http://www.zillo.com', 'my link to zillow', 5),
+  ('Realtor', 'http://www.realtor.com', 'my link to Realtor', 1),
+  ('Instagram', 'http://www.instagram.com', 'my link to Instagram', 2),
+  ('YouTube', 'http://www.youtube.com', 'my link to YouTube', 5);
